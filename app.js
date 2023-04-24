b928b795597f0a9f6745d19e33ed6e473951e6c6
Omar Alhamad <omar93@hotmail.se>
2023-04-24 15:32:37 +0200

    2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!! from openshift')
+    res.send('Hello World! updating only the app.js file and pushing to github')
 })
 
 app.listen(port, () => {
