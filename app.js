66f3c42697b8760d6503eae46336b0b2b264c877
Omar Alhamad <omar93@hotmail.se>
2023-04-27 23:26:11 +0200

    testing cicd

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello World! leaving work. hope it udates when i'm home")
+    res.send("Hello World! ARGOCD!")
 })
 
 app.listen(port, () => {
