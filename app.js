2238f21c3046d78dd02af3a07bd6e026bd82934d
Omar Alhamad <omar93@hotmail.se>
2023-04-27 23:36:07 +0200

    testing new rollout

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello World! ARGOCD!")
+    res.send("Hello World! ARGOCD_PATCH2")
 })
 
 app.listen(port, () => {
