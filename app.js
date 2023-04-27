a979a9625fa6116270355db3a15bb2b66ca0e871
Omar Alhamad <omar93@hotmail.se>
2023-04-27 23:42:04 +0200

    patch 3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("Hello World! ARGOCD_PATCH2")
+    res.send("Hello World! ARGOCD_PATCH3")
 })
 
 app.listen(port, () => {
